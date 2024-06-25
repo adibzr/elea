ccb3323acbaf6c64f19d72d74a5d7606672c3330
adibzr <adibraed1gmail.com>
2024-06-24 22:01:06 -0300

    m,j mn

--- script.js
+++ script.js
@@ -1,195 +1,240 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const conversations = [
-    {
-      title: "Conversación 1 - Título largo",
-      chatMessages: [
-        "¡Hola! Quiero saber información sobre la siguiente molécula: adrenalina",
-        "La adrenalina, también conocida como epinefrina, es una hormona y un neurotransmisor crucial en la respuesta de 'lucha o huida' del cuerpo humano. Aquí hay una descripción detallada:",
-      ],
-      date: "2024-06-22",
-    },
-    {
-      title: "Conversación 2",
-      chatMessages: [],
-      date: "2024-06-19",
-    },
-    {
-      title: "Conversación 3 - Título largo",
-      chatMessages: [],
-      date: "2024-06-2",
-    },
-  ];
+const conversations = [
+  {
+    title: "Conversación 1 - Título largo",
+    chatMessages: [
+      "¡Hola! Quiero saber información sobre la siguiente molécula: adrenalina",
+      "La adrenalina, también conocida como epinefrina, es una hormona y un neurotransmisor crucial en la respuesta de 'lucha o huida' del cuerpo humano. Aquí hay una descripción detallada:",
+    ],
+    date: "2024-06-24",
+  },
+  {
+    title: "Conversación 2",
+    chatMessages: [],
+    date: "2024-06-19",
+  },
+  {
+    title: "Conversación 2",
+    chatMessages: [],
+    date: "2024-06-23",
+  },
+  {
+    title: "Conversación 3 - Título largo",
+    chatMessages: [],
+    date: "2024-06-2",
+  },
+  {
+    title: "Conversación 3 - Título largo",
+    chatMessages: [],
+    date: "2024-05-2",
+  },
+];
+const nodes = Array.from(aiMessage.childNodes);
 
+const menuItems = {
+  today: document.getElementById("hoy"),
+  yesterday: document.getElementById("ayer"),
+  prev7days: document.getElementById("7dias"),
+  prev30days: document.getElementById("30dias"),
+  older: document.getElementById("anterior"),
+};
+const searchInput = document.getElementById("searchInput");
+const searchIcon = document.getElementById("searchIcon");
+
+/**
+ * Reveals nodes one by one in a chat message.
+ *
+ * @return {void} This function does not return anything.
+ */
+
+function revealNode(node) {
   const aiMessage = document.getElementById("aiMessage");
-  const nodes = Array.from(aiMessage.childNodes);
-  let currentIndex = 0;
-
   aiMessage.innerHTML = "";
   const cursor = document.createElement("span");
   cursor.className = "cursor";
   aiMessage.appendChild(cursor);
-
-  function revealNode() {
-    if (currentIndex < nodes.length) {
-      cursor.style.display = "inline-block";
-      const node = nodes[currentIndex];
+  cursor.style.display = "inline-block";
+  nodes.forEach(async (node, index) => {
+    setTimeout(() => {
       aiMessage.insertBefore(node, cursor);
-      currentIndex++;
-      setTimeout(revealNode, 500);
-    } else {
-      cursor.remove();
-    }
+      if (index === nodes.length - 1) {
+        cursor.remove();
+        send.style.display = "block";
+        loading.style.display = "none";
+      }
+    }, index * 300);
+  });
+}
+
+/**
+ * Creates a conversation element with a title and appends it to the menu container.
+ *
+ * @param {string} title - The title of the conversation.
+ * @param {string[]} chatMessages - The chat messages of the conversation.
+ * @param {string} date - The date of the conversation.
+ * @return {HTMLElement} The created conversation element.
+ */
+function createConversation(title, chatMessages = [], date) {
+  const container = document.getElementById("menuConteiner");
+  const div = document.createElement("div");
+  div.className = "dropend menuItem";
+
+  const conversation = document.createElement("button");
+  conversation.type = "button";
+  conversation.className = "btn chat-title";
+  conversation.textContent = title;
+
+  const dropdownButton = document.createElement("button");
+  dropdownButton.type = "button";
+  dropdownButton.className =
+    "btn dropdown-button dropdown-toggle dropdown-toggle-split";
+  dropdownButton.setAttribute("data-bs-toggle", "dropdown");
+  dropdownButton.setAttribute("aria-haspopup", "true");
+  dropdownButton.setAttribute("aria-expanded", "false");
+  dropdownButton.setAttribute("data-bs-offset", `0,10`);
+
+  const svgNS = "http://www.w3.org/2000/svg";
+
+  const svg = document.createElementNS(svgNS, "svg");
+  svg.setAttribute("width", "24");
+  svg.setAttribute("height", "20");
+  svg.setAttribute("viewBox", "0 0 24 24");
+  svg.setAttribute("fill", "none");
+  svg.setAttribute("xmlns", svgNS);
+
+  const path = document.createElementNS(svgNS, "path");
+  path.setAttribute("id", "menuItemButton");
+  path.setAttribute(
+    "d",
+    "M6 14C5.45 14 4.97917 13.8042 4.5875 13.4125C4.19583 13.0208 4 12.55 4 12C4 11.45 4.19583 10.9792 4.5875 10.5875C4.97917 10.1958 5.45 10 6 10C6.55 10 7.02083 10.1958 7.4125 10.5875C7.80417 10.9792 8 11.45 8 12C8 12.55 7.80417 13.0208 7.4125 13.4125C7.02083 13.8042 6.55 14 6 14ZM12 14C11.45 14 10.9792 13.8042 10.5875 13.4125C10.1958 13.0208 10 12.55 10 12C10 11.45 10.1958 10.9792 10.5875 10.5875C10.9792 10.1958 11.45 10 12 10C12.55 10 13.0208 10.1958 13.4125 10.5875C13.8042 10.9792 14 11.45 14 12C14 12.55 13.8042 13.0208 13.4125 13.4125C13.0208 13.8042 12.55 14 12 14ZM18 14C17.45 14 16.9792 13.8042 16.5875 13.4125C16.1958 13.0208 16 12.55 16 12C16 11.45 16.1958 10.9792 16.5875 10.5875C16.9792 10.1958 17.45 10 18 10C18.55 10 19.0208 10.1958 19.4125 10.5875C19.8042 10.9792 20 11.45 20 12C20 12.55 19.8042 13.0208 19.4125 13.4125C19.0208 13.8042 18.55 14 18 14Z"
+  );
+  path.setAttribute("fill", "#1E2430");
+
+  svg.appendChild(path);
+  dropdownButton.appendChild(svg);
+
+  div.appendChild(conversation);
+  div.appendChild(dropdownButton);
+
+  const dropdownMenu = document.getElementById("dropdownMenu");
+  const clonedElement = dropdownMenu.cloneNode(true);
+
+  div.appendChild(clonedElement);
+
+  container.appendChild(div);
+
+  return { node: div, date };
+}
+
+/**
+ * Categorizes conversations into different time periods and appends them to the appropriate chat group.
+ * @param {Object} conversation
+ * @return {void}
+ */
+function categorizeConversation(conversation) {
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+  const prev7days = new Date(today);
+  prev7days.setDate(today.getDate() - 7);
+  const prev30days = new Date(today);
+  prev30days.setDate(today.getDate() - 30);
+
+  const dateParts = conversation.date.split("-");
+  const convDate = new Date(dateParts[0], dateParts[1] - 1, dateParts[2], 12);
+  if (convDate.toDateString() === today.toDateString()) {
+    menuItems.today.appendChild(conversation.node);
+  } else if (convDate.toDateString() === yesterday.toDateString()) {
+    menuItems.yesterday.appendChild(conversation.node);
+  } else if (convDate > prev7days) {
+    menuItems.prev7days.appendChild(conversation.node);
+  } else if (convDate > prev30days) {
+    menuItems.prev30days.appendChild(conversation.node);
+  } else {
+    menuItems.older.appendChild(conversation.node);
   }
-  const menuItems = {
-    today: document.getElementById("hoy"),
-    yesterday: document.getElementById("ayer"),
-    prev7days: document.getElementById("7dias"),
-    prev30days: document.getElementById("30dias"),
-    older: document.getElementById("anterior"),
-  };
-  const searchInput = document.getElementById("searchInput");
-  const searchIcon = document.getElementById("searchIcon");
+}
 
-  /**
-   * Creates a conversation element with a title and appends it to the menu container.
-   *
-   * @param {string} title - The title of the conversation.
-   * @param {string[]} chatMessages - The chat messages of the conversation.
-   * @param {string} date - The date of the conversation.
-   * @return {HTMLElement} The created conversation element.
-   */
-  function createConversation(title, chatMessages = [], date) {
-    const container = document.getElementById("menuConteiner");
-    const div = document.createElement("div");
-    div.className = "dropend menuItem";
-
-    const conversation = document.createElement("button");
-    conversation.type = "button";
-    conversation.className = "btn chat-title";
-    conversation.textContent = title;
-
-    const dropdownButton = document.createElement("button");
-    dropdownButton.type = "button";
-    dropdownButton.className =
-      "btn dropdown-button dropdown-toggle dropdown-toggle-split";
-    dropdownButton.setAttribute("data-bs-toggle", "dropdown");
-    dropdownButton.setAttribute("aria-haspopup", "true");
-    dropdownButton.setAttribute("aria-expanded", "false");
-    dropdownButton.setAttribute("data-bs-offset", `0,10`);
-
-    const svgNS = "http://www.w3.org/2000/svg";
-
-    const svg = document.createElementNS(svgNS, "svg");
-    svg.setAttribute("width", "24");
-    svg.setAttribute("height", "20");
-    svg.setAttribute("viewBox", "0 0 24 24");
-    svg.setAttribute("fill", "none");
-    svg.setAttribute("xmlns", svgNS);
-
-    const path = document.createElementNS(svgNS, "path");
-    path.setAttribute("id", "menuItemButton");
-    path.setAttribute(
-      "d",
-      "M6 14C5.45 14 4.97917 13.8042 4.5875 13.4125C4.19583 13.0208 4 12.55 4 12C4 11.45 4.19583 10.9792 4.5875 10.5875C4.97917 10.1958 5.45 10 6 10C6.55 10 7.02083 10.1958 7.4125 10.5875C7.80417 10.9792 8 11.45 8 12C8 12.55 7.80417 13.0208 7.4125 13.4125C7.02083 13.8042 6.55 14 6 14ZM12 14C11.45 14 10.9792 13.8042 10.5875 13.4125C10.1958 13.0208 10 12.55 10 12C10 11.45 10.1958 10.9792 10.5875 10.5875C10.9792 10.1958 11.45 10 12 10C12.55 10 13.0208 10.1958 13.4125 10.5875C13.8042 10.9792 14 11.45 14 12C14 12.55 13.8042 13.0208 13.4125 13.4125C13.0208 13.8042 12.55 14 12 14ZM18 14C17.45 14 16.9792 13.8042 16.5875 13.4125C16.1958 13.0208 16 12.55 16 12C16 11.45 16.1958 10.9792 16.5875 10.5875C16.9792 10.1958 17.45 10 18 10C18.55 10 19.0208 10.1958 19.4125 10.5875C19.8042 10.9792 20 11.45 20 12C20 12.55 19.8042 13.0208 19.4125 13.4125C19.0208 13.8042 18.55 14 18 14Z"
-    );
-    path.setAttribute("fill", "#1E2430");
-
-    svg.appendChild(path);
-    dropdownButton.appendChild(svg);
-
-    div.appendChild(conversation);
-    div.appendChild(dropdownButton);
-
-    const dropdownMenu = document.getElementById("dropdownMenu");
-    const clonedElement = dropdownMenu.cloneNode(true);
-
-    div.appendChild(clonedElement);
-
-    container.appendChild(div);
-
-    return { node: div, date };
-  }
-
-  /**
-   * Categorizes conversations into different time periods and appends them to the appropriate chat group.
-   * @param {Object} conversation
-   * @return {void}
-   */
-  function categorizeConversation(conversation) {
-    const today = new Date();
-    const yesterday = new Date(today);
-    yesterday.setDate(today.getDate() - 1);
-    const prev7days = new Date(today);
-    prev7days.setDate(today.getDate() - 7);
-    const prev30days = new Date(today);
-    prev30days.setDate(today.getDate() - 30);
-
-    const dateParts = conversation.date.split("-");
-    const convDate = new Date(dateParts[0], dateParts[1] - 1, dateParts[2], 12);
-    if (convDate.toDateString() === today.toDateString()) {
-      menuItems.today.appendChild(conversation.node);
-    } else if (convDate.toDateString() === yesterday.toDateString()) {
-      menuItems.yesterday.appendChild(conversation.node);
-    } else if (convDate > prev7days) {
-      menuItems.prev7days.appendChild(conversation.node);
-    } else if (convDate > prev30days) {
-      menuItems.prev30days.appendChild(conversation.node);
-    } else {
-      menuItems.older.appendChild(conversation.node);
-    }
-  }
-
-  /**
-   * Adds event listeners to each conversation item and to the search icon.
-   * When the search icon is clicked, it filters the menu items based on the value of the search input.
-   * @return {void}
-   */
-  function addEventListeners() {
-    // Add click event listeners to each conversation item
-    document.querySelectorAll(".menu > ul > li").forEach((item) => {
-      item.addEventListener("click", (item) => {
-        console.log(item.target.innerHTML);
-      });
+/**
+ * Adds event listeners to each conversation item and to the search icon.
+ * When the search icon is clicked, it filters the menu items based on the value of the search input.
+ * @return {void}
+ */
+function addEventListeners() {
+  // Add click event listeners to each conversation item
+  Object.values(menuItems).forEach((item) => {
+    item.addEventListener("click", (item) => {
+      item.target.className += " active";
+      console.log(item.target.innerHTML);
     });
-
-    // Add click event listener to the search icon and filters conversations
-    searchIcon.addEventListener("click", () => {
-      const filter = searchInput.value.toLowerCase();
-      menuItems.forEach((item) => {
-        const text = item.textContent.toLowerCase();
-        if (text.includes(filter)) {
-          item.style.display = "";
-        } else {
-          item.style.display = "none";
-        }
-      });
-    });
-    const generarReporte = document.getElementById("generate-reporte");
-    const analizando = document.getElementById("aiAnalizando");
-    generarReporte.addEventListener("click", () => {
-      analizando.style.display = "grid";
-      generarReporte.setAttribute("disabled", true);
-      // generarReporte.className += "disabled";
-      setTimeout(() => {
-        analizando.style.display = "none";
-        generarReporte.removeAttribute("disabled");
-      }, 3000);
-    });
-  }
-
-  conversations.forEach((conversation) => {
-    const date = conversation.date;
-    const chat = createConversation(
-      conversation.title,
-      conversation.chatMessages,
-      date
-    );
-    categorizeConversation(chat, date);
   });
 
-  addEventListeners();
+  // Add click event listener to the search icon and filters conversations
 
-  revealNode();
+  Object.values(menuItems).forEach((item) => {
+    const text = item.querySelectorAll("button")[0].innerText;
+    console.log(text);
+  });
+
+  // Add click event listener to the search icon and filters conversations
+  searchIcon.addEventListener("click", () => {
+    const filter = searchInput.value.toLowerCase();
+    Object.values(menuItems).forEach((item) => {
+      const text = item.textContent.toLowerCase();
+      if (text.includes(filter)) {
+        item.style.display = "";
+      } else {
+        item.style.display = "none";
+      }
+    });
+
+    hideEmptyMenu();
+  });
+
+  //add click event to send button
+  const send = document.getElementById("send");
+  const loading = document.getElementById("loading");
+  send.addEventListener("click", () => {
+    const message = document.getElementById("chatInput").value;
+    send.style.display = "none";
+    loading.style.display = "block";
+  });
+
+  //add event listener to generate report button
+  const generarReporte = document.getElementById("generate-reporte");
+  const analizando = document.getElementById("aiAnalizando");
+  generarReporte.addEventListener("click", () => {
+    analizando.style.display = "grid";
+    generarReporte.setAttribute("disabled", true);
+    setTimeout(() => {
+      analizando.style.display = "none";
+      generarReporte.removeAttribute("disabled");
+    }, 3000);
+  });
+}
+
+function hideEmptyMenu() {
+  document.querySelectorAll(".menu > span").forEach((span) => {
+    const ul = span.nextElementSibling;
+    if (ul.style.display === "none") {
+      span.style.display = "none";
+    } else {
+      span.style.display = "block";
+    }
+  });
+}
+
+function loadConversations(conversation) {
+  conversation.forEach((conv) => {
+    const date = conv.date;
+    const chat = createConversation(conv.title, conv.chatMessages, date);
+    categorizeConversation(chat, date);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadConversations(conversations);
+  addEventListeners();
+  revealNode(nodes);
 
   //helpers
   // Hide empty sections
